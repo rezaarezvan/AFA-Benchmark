4169a87024b13a6ea2b0134fe60fa99ad38d03c6
g4alllf <wuhan1505@gmail.com>
2025-04-14 16:24:28 +0200

    feat: add reimplementation of GDFS method

---
+++ src/afa_discriminative/scripts/run.py
@@ -0,0 +1,141 @@
+import os
+import torch
+import pickle
+import argparse
+import numpy as np
+import torch.nn as nn
+from tqdm.auto import tqdm
+from torch.utils.data import DataLoader
+from torchmetrics import Accuracy, AUROC
+from afa_discriminative.utils import MaskLayer
+from afa_discriminative.datasets import load_spam, load_diabetes, load_miniboone, data_split
+from afa_discriminative.models import MaskingPretrainer
+from afa_discriminative.afa_methods import GreedyDynamicSelection
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--dataset', type=str, default='spam',
+                    choices=['spam', 'diabetes', 'miniboone', 'concrete'])
+parser.add_argument('--method', type=str, default='greedy',
+                    choices=['greedy'])
+parser.add_argument('--gpu', type=int, default=0)
+parser.add_argument('--num_trials', type=int, default=1)
+parser.add_argument('--num_restarts', type=int, default=1)
+
+
+# Various configurations.
+load_data_dict = {
+    'spam': load_spam,
+    'diabetes': load_diabetes,
+    'miniboone': load_miniboone,
+}
+num_features_dict = {
+    'spam': list(range(1, 11)) + list(range(15, 30, 5)),
+    'diabetes': list(range(1, 11)),
+    'miniboone': list(range(1, 11)) + list(range(15, 30, 5)),
+}
+max_features_dict = {
+    'spam': 35,
+    'diabetes': 35,
+    'miniboone': 35,
+}
+
+
+# Helper function for network architecture.
+def get_network(d_in, d_out):
+    hidden = 128
+    dropout = 0.3
+    model = nn.Sequential(
+        nn.Linear(d_in, hidden),
+        nn.ReLU(),
+        nn.Dropout(dropout),
+        nn.Linear(hidden, hidden),
+        nn.ReLU(),
+        nn.Dropout(dropout),
+        nn.Linear(hidden, d_out))
+    return model
+
+
+if __name__ == '__main__':
+    # Parse args.
+    args = parser.parse_args()
+    load_data = load_data_dict[args.dataset]
+    num_features = num_features_dict[args.dataset]
+    device = torch.device('cuda', args.gpu)
+    
+    # Load dataset.
+    dataset = load_data()
+    d_in = dataset.input_size
+    d_out = dataset.output_size
+    
+    # Normalize and split dataset.
+    mean = dataset.tensors[0].mean(dim=0)
+    std = torch.clamp(dataset.tensors[0].std(dim=0), min=1e-3)
+    
+    dataset.tensors = (dataset.tensors[0] - mean, dataset.tensors[1])
+    train_dataset, val_dataset, test_dataset = data_split(dataset)
+    
+    # Prepare dataloaders.
+    train_loader = DataLoader(
+        train_dataset, batch_size=128,
+        shuffle=True, pin_memory=True, drop_last=True)
+    val_loader = DataLoader(val_dataset, batch_size=1024, pin_memory=True)
+    test_loader = DataLoader(test_dataset, batch_size=1024, pin_memory=True)
+    
+    # Make results directory.
+    if not os.path.exists('results'):
+        os.makedirs('results')
+    
+    for trial in range(args.num_trials):
+        # For saving results.
+        results_dict = {
+            'auroc': {},
+            'acc': {},
+            'features': {}
+        }
+        auroc_metric = lambda pred, y: AUROC(task='multiclass', num_classes=d_out)(pred.softmax(dim=1), y)
+        acc_metric = Accuracy(task='multiclass', num_classes=d_out)
+        
+        if args.method == 'greedy':
+            # Prepare networks.
+            predictor = get_network(d_in * 2, d_out)
+            selector = get_network(d_in * 2, d_in)
+            
+            # Pretrain predictor.
+            mask_layer = MaskLayer(append=True)
+            pretrain = MaskingPretrainer(predictor, mask_layer).to(device)
+            pretrain.fit(
+                train_loader,
+                val_loader,
+                lr=1e-3,
+                nepochs=100,
+                loss_fn=nn.CrossEntropyLoss(),
+                patience=5,
+                verbose=True)
+            
+            # Train selector and predictor jointly.
+            gdfs = GreedyDynamicSelection(selector, predictor, mask_layer).to(device)
+            gdfs.fit(
+                train_loader,
+                val_loader,
+                lr=1e-3,
+                nepochs=250,
+                max_features=max_features_dict[args.dataset],
+                loss_fn=nn.CrossEntropyLoss(),
+                patience=5,
+                verbose=True)
+            
+            # Evaluate.
+            for num in num_features:
+                auroc, acc = gdfs.evaluate(test_loader, num, (auroc_metric, acc_metric))
+                results_dict['auroc'][num] = auroc
+                results_dict['acc'][num] = acc
+                print(f'Num = {num}, AUROC = {100*auroc:.2f}, Acc = {100*acc:.2f}')
+                
+            # Save model
+            gdfs.cpu()
+            torch.save(gdfs, f'results/{args.dataset}_{args.method}_{trial}.pt')
+        
+        # Save results.
+        with open(f'results/{args.dataset}_{args.method}_{trial}.pkl', 'wb') as f:
+            pickle.dump(results_dict, f)
---
+++ src/afa_discriminative/utils.py
@@ -0,0 +1,60 @@
+import torch
+import torch.nn as nn
+from torch.distributions import RelaxedOneHotCategorical
+
+
+def restore_parameters(model, best_model):
+    '''Move parameters from best model to current model.'''
+    for param, best_param in zip(model.parameters(), best_model.parameters()):
+        param.data = best_param
+
+
+def generate_uniform_mask(batch_size, num_features):
+    '''Generate binary masks with cardinality chosen uniformly at random.'''
+    unif = torch.rand(batch_size, num_features)
+    ref = torch.rand(batch_size, 1)
+    return (unif > ref).float()
+
+
+def make_onehot(x):
+    '''Make an approximately one-hot vector one-hot.'''
+    argmax = torch.argmax(x, dim=1)
+    onehot = torch.zeros(x.shape, dtype=x.dtype, device=x.device)
+    onehot[torch.arange(len(x)), argmax] = 1
+    return onehot
+
+
+class MaskLayer(nn.Module):
+    '''
+    Mask layer for tabular data.
+    
+    Args:
+      append:
+      mask_size:
+    '''
+    def __init__(self, append, mask_size=None):
+        super().__init__()
+        self.append = append
+        self.mask_size = mask_size
+
+    def forward(self, x, m):
+        out = x * m
+        if self.append:
+            out = torch.cat([out, m], dim=1)
+        return out
+    
+
+class ConcreteSelector(nn.Module):
+    '''Output layer for selector models.'''
+
+    def __init__(self, gamma=0.2):
+        super().__init__()
+        self.gamma = gamma
+
+    def forward(self, logits, temp, deterministic=False):
+        if deterministic:
+            # TODO this is somewhat untested, but seems like best way to preserve argmax
+            return torch.softmax(logits / (self.gamma * temp), dim=-1)
+        else:
+            dist = RelaxedOneHotCategorical(temp, logits=logits / self.gamma)
+            return dist.rsample()
