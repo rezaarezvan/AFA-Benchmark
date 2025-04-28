96f7caac78d1f9a4d2477b9a4130b627b2a1041f
Linus Aronsson <linaro@chalmers.se>
2025-04-28 18:38:29 +0200

    added n_classes as static var to all datasets

--- src/common/scripts/generate_dummy_afa_methods.py
+++ src/common/scripts/generate_dummy_afa_methods.py
@@ -4,66 +4,99 @@ Generates .pt files for the two dummy methods SequentialDummyAFAMethod and Rando
 
 
 import argparse
+from datetime import datetime
+from pathlib import Path
 import torch
 from tqdm import tqdm
 import os
 
 from afa_rl.afa_methods import RandomDummyAFAMethod, SequentialDummyAFAMethod
-from common.registry import AFA_DATASET_REGISTRY
+from common.registry import AFA_DATASET_REGISTRY, AFA_METHOD_REGISTRY
+
+
+def save_dummy_method(method_name, hard_budget, dataset_train_path, dataset_val_path):
+    # Create folders if necessary
+    # (args.models_folder / method_name).mkdir(parents=True, exist_ok=True)
+
+    # Find method class using registry
+    method_cls = AFA_METHOD_REGISTRY[method_name]
+
+    # Find dataset type from the dataset path
+    dataset_type = dataset_train_path.parent.name
+    # Find dataset class using registry
+    dataset_cls = AFA_DATASET_REGISTRY[dataset_type]
+
+    # Create five methods
+    for i in tqdm(range(1, 6)):
+        # Generate a unique datetime string and create folders
+        timestr = datetime.now().strftime("%Y%m%d_%H%M%S")
+        (args.models_folder / method_name / timestr).mkdir(
+            parents=True, exist_ok=True
+        )
+
+        method = method_cls(
+            device=torch.device("cpu"),
+            n_classes=dataset_cls.n_classes,
+        )
+        method.save(
+            (args.models_folder / method_name / timestr / "model.pt")
+        )
+        # Also save a yaml file with params
+        with open(
+            (args.models_folder / method_name / timestr / "params.yaml"), "w"
+        ) as f:
+            f.write(f"hard_budget: {hard_budget}\n")
+            f.write(f"train_dataset_path: {dataset_train_path}\n")
+            f.write(f"val_dataset_path: {dataset_val_path}\n")
+
 
 
 def main(args: argparse.Namespace):
-    # Create args.models_folder if necessary
-    os.makedirs(args.models_folder, exist_ok=True)
-    # Create subfolders for the two methods if necessary
-    os.makedirs(os.path.join(args.models_folder, "sequential_dummy"), exist_ok=True)
-    os.makedirs(os.path.join(args.models_folder, "random_dummy"), exist_ok=True)
-    for i in tqdm(range(1, 6)):
-        method = SequentialDummyAFAMethod(
-            device=torch.device("cpu"),
-            n_classes=args.n_classes,
-        )
-        method.save(
-            os.path.join(
-                args.models_folder,
-                "sequential_dummy",
-                f"sequential_dummy-{args.dataset_type}_train_split_{i}.pt",
-            )
-        )
+    # The dataset type is the parent folder of dataset_train_path
+    args.dataset_type = args.dataset_train_path.parent.name
+    # Training and validation datasets should have the same type
+    assert args.dataset_type == args.dataset_val_path.parent.name
 
-        method = RandomDummyAFAMethod(
-            device=torch.device("cpu"),
-            n_classes=args.n_classes,
-        )
-        method.save(
-            os.path.join(
-                args.models_folder,
-                "random_dummy",
-                f"random_dummy-{args.dataset_type}_train_split_{i}.pt",
-            )
-        )
+    save_dummy_method(
+        "sequential_dummy",
+        args.hard_budget,
+        args.dataset_train_path,
+        args.dataset_val_path,
+    )
+
+    save_dummy_method(
+        "random_dummy",
+        args.hard_budget,
+        args.dataset_train_path,
+        args.dataset_val_path,
+    )
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--models_folder",
-        type=str,
-        required=True,
+        type=Path,
+        default="models",
         help="Path to the models folder",
     )
     parser.add_argument(
-        "--dataset_type",
-        type=str,
-        required=True,
-        choices=AFA_DATASET_REGISTRY.keys(),
-        help="Name of the dataset (must be one of the registered types)",
+        "--dataset_train_path",
+        type=Path,
+        default="data/cube/train_split_1.pt",
+        help="Which dataset the model is trained on",
     )
     parser.add_argument(
-        "--n_classes",
+        "--dataset_val_path",
+        type=Path,
+        default="data/cube/val_split_1.pt",
+        help="Which dataset the model is validated on",
+    )
+    parser.add_argument(
+        "--hard_budget",
         type=int,
-        required=True,
-        help="Number of classes in the dataset",
+        default=10,
+        help="Hard budget for the method",
     )
     args = parser.parse_args()
     main(args)
--- src/common/scripts/generate_dummy_afa_methods.sh
+++
@@ -1,4 +0,0 @@
-uv run src/common/scripts/generate_dummy_afa_methods.py \
-    --models_folder models \
-    --dataset_type cube \
-    --n_classes 8
