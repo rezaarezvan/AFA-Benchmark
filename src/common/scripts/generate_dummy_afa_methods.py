bfd8763b090723c1b32e24257eb02d3ca4794849
Valter Schutz <valterschutz@proton.me>
2025-04-29 14:35:12 +0200

    fix: generate dummy methods with different seeds

--- src/common/scripts/generate_dummy_afa_methods.py
+++ src/common/scripts/generate_dummy_afa_methods.py
@@ -6,6 +6,7 @@ Generates .pt files for the two dummy methods SequentialDummyAFAMethod and Rando
 import argparse
 from datetime import datetime
 from pathlib import Path
+from sklearn.model_selection import train_test_split
 import torch
 from tqdm import tqdm
 import os
@@ -14,12 +15,10 @@ import yaml
 
 from afa_rl.afa_methods import RandomDummyAFAMethod, SequentialDummyAFAMethod
 from common.registry import AFA_DATASET_REGISTRY, AFA_METHOD_REGISTRY
+from coolname import generate_slug
 
 
-def save_dummy_method(method_name, hard_budget, dataset_train_path, dataset_val_path):
-    # Create folders if necessary
-    # (args.models_folder / method_name).mkdir(parents=True, exist_ok=True)
-
+def save_dummy_method(method_name: str, hard_budget: int, dataset_train_path: Path, dataset_val_path: Path, seed: int):
     # Find method class using registry
     method_cls = AFA_METHOD_REGISTRY[method_name]
 
@@ -28,57 +27,61 @@ def save_dummy_method(method_name, hard_budget, dataset_train_path, dataset_val_
     # Find dataset class using registry
     dataset_cls = AFA_DATASET_REGISTRY[dataset_type]
 
-    # Create five methods
-    for i in tqdm(range(1, 6)):
-        # Generate a unique datetime string and create folders
-        timestr = datetime.now().strftime("%Y%m%d_%H%M%S")
-        (args.models_folder / method_name / timestr).mkdir(
-            parents=True, exist_ok=True
-        )
+    # Generate a unique datetime string and create folders
+    timestr = generate_slug(2)
+    (args.models_folder / method_name / timestr).mkdir(
+        parents=True, exist_ok=True
+    )
 
-        method = method_cls(
-            device=torch.device("cpu"),
-            n_classes=dataset_cls.n_classes,
-        )
-        method.save(
-            (args.models_folder / method_name / timestr / "model.pt")
-        )
-        # Also save a yaml file with params
-        with open(
-            (args.models_folder / method_name / timestr / "params.yml"), "w"
-        ) as f:
-            # f.write(f"hard_budget: {hard_budget}\n")
-            # f.write(f"train_dataset_path: {dataset_train_path}\n")
-            # f.write(f"val_dataset_path: {dataset_val_path}\n")
-            yaml.dump({
-                "hard_budget": hard_budget,
-                "train_dataset_path": str(dataset_train_path),
-                "val_dataset_path": str(dataset_val_path),
-            }, f, default_flow_style=False)
+    method = method_cls(
+        device=torch.device("cpu"),
+        n_classes=dataset_cls.n_classes,
+    )
+    method.save(
+        (args.models_folder / method_name / timestr / "model.pt")
+    )
+    # Also save a yaml file with params
+    with open(
+        (args.models_folder / method_name / timestr / "params.yml"), "w"
+    ) as f:
+        yaml.dump({
+            "hard_budget": hard_budget,
+            "train_dataset_path": str(dataset_train_path),
+            "val_dataset_path": str(dataset_val_path),
+            "seed": seed,
+        }, f, default_flow_style=False)
 
 
 
 def main(args: argparse.Namespace):
-    # The dataset type is the parent folder of dataset_train_path
-    args.dataset_type = args.dataset_train_path.parent.name
-    # Training and validation datasets should have the same type
-    assert args.dataset_type == args.dataset_val_path.parent.name
+    # The dataset type is the name of the folder containing the dataset
+    args.dataset_type = args.dataset_folder.name
 
     print("Generating dummy methods...")
-    save_dummy_method(
-        "sequential_dummy",
-        args.hard_budget,
-        args.dataset_train_path,
-        args.dataset_val_path,
-    )
+    # Create a method for each (train_path, val_path) and seed combination
+    for split in range(1, args.n_splits+1):
+        train_path = args.dataset_folder / f"train_split_{split}.pt"
+        val_path = args.dataset_folder / f"val_split_{split}.pt"
 
-    save_dummy_method(
-        "random_dummy",
-        args.hard_budget,
-        args.dataset_train_path,
-        args.dataset_val_path,
-    )
+        # The data should exist
+        assert train_path.exists() and val_path.exists()
 
+        for seed in range(1, args.n_seeds+1):
+            save_dummy_method(
+                "sequential_dummy",
+                args.hard_budget,
+                train_path,
+                val_path,
+                seed
+            )
+
+            save_dummy_method(
+                "random_dummy",
+                args.hard_budget,
+                train_path,
+                val_path,
+                seed
+            )
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -89,22 +92,28 @@ if __name__ == "__main__":
         help="Path to the models folder",
     )
     parser.add_argument(
-        "--dataset_train_path",
+        "--dataset_folder",
         type=Path,
-        default="data/cube/train_split_1.pt",
+        default=Path("data/cube"),
         help="Which dataset the model is trained on",
     )
     parser.add_argument(
-        "--dataset_val_path",
-        type=Path,
-        default="data/cube/val_split_1.pt",
-        help="Which dataset the model is validated on",
+        "--n_splits",
+        type=int,
+        default=3,
+        help="Number of splits for the dataset",
     )
     parser.add_argument(
         "--hard_budget",
         type=int,
         default=10,
         help="Hard budget for the method",
+    )
+    parser.add_argument(
+        "--n_seeds",
+        type=int,
+        default=5,
+        help="How many different seeds to use",
     )
     args = parser.parse_args()
     main(args)
