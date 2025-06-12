afc1d3448016fe32b377f266aa616ca387e88c25
Valter Schutz <valterschutz@proton.me>
2025-06-12 17:15:54 +0200

    fix: manually listing evaluation results in plotting script?

--- scripts/pipelines/plot.fish
+++ scripts/pipelines/plot.fish
@@ -9,44 +9,16 @@ set -gx WANDB_PROJECT afa-benchmark
 # Parse args
 # -----------------------
 
-argparse "dataset=+" "budgets=+" "method=+" "split=+" "help" "evaluation-alias=?" "output-alias=?" "wandb-entity=?" "wandb-project=?" -- $argv
+argparse "help" "output-alias=?" "wandb-entity=?" "wandb-project=?" -- $argv
 or exit 1
 
 # Print help if specified
 if set -ql _flag_help
-    echo "Usage: plot.fish --dataset=<str> --budgets=<str> --method=<str> --split=<int> [--help] [--evaluation-alias=<str>] [--output-alias=<str>] [--wandb-entity=<str>] [--wandb-project=<str>]" >&2
+    echo "Usage: plot.fish [--help] [--output-alias=<str>] [--wandb-entity=<str>] [--wandb-project=<str>]" >&2
     exit 1
 end
 
 # Default arguments
-
-# No default arguments for datasets
-if set -ql _flag_dataset
-    set datasets $_flag_dataset
-else
-    echo "datasets must be set"
-    exit 1
-end
-
-# No default argument for budgets
-if set -ql _flag_budgets
-    set budgets $_flag_budgets
-else
-    echo "budgets must be set"
-    exit 1
-end
-
-set -l methods shim2018 randomdummy
-set -ql _flag_method
-and set methods $_flag_method
-
-set -l splits 1 2
-set -ql _flag_split
-and set splits $_flag_split
-
-set -l evaluation_alias tmp
-set -ql _flag_evaluation_alias
-and set evaluation_alias $_flag_evaluation_alias
 
 set -l output_alias tmp
 set -ql _flag_output_alias
@@ -67,20 +39,79 @@ set -gx WANDB_PROJECT $wandb_project
 echo "Starting plotting job..."
 sleep 1
 
-set -l eval_artifact_names
-set -l i 1
-for dataset in $datasets
-    set -l dataset_budgets $budgets[$i]
-    for split in $splits
-        for classifier_artifact_name in "builtin" "masked_mlp_classifier-$dataset""_split_$split"
-            for budget in (string split , $dataset_budgets)
-                for method in $methods
-                    set -a eval_artifact_names train_$method-{$dataset}_split_$split-budget_$budget-seed_42-$classifier_artifact_name:$evaluation_alias
-                end
-            end
-        end
-    end
-    set i (math "$i+1")
-end
+# Automated approach
+# set -l eval_artifact_names
+# set -l i 1
+# for dataset in $datasets
+#     set -l dataset_budgets $budgets[$i]
+#     for split in $splits
+#         for classifier_artifact_name in "builtin" "masked_mlp_classifier-$dataset""_split_$split"
+#             for budget in (string split , $dataset_budgets)
+#                 for method in $methods
+#                     set -a eval_artifact_names train_$method-{$dataset}_split_$split-budget_$budget-seed_42-$classifier_artifact_name:$evaluation_alias
+#                 end
+#             end
+#         end
+#     end
+#     set i (math "$i+1")
+# end
+
+# Manual approach
+set -l eval_artifact_names \
+    train_shim2018_cube_split_1-budget_3_seed_42-masked_mlp_classifier-cube_split_1:Jun12 \
+    train_shim2018_cube_split_1-budget_5_seed_42-masked_mlp_classifier-cube_split_1:Jun12 \
+    train_shim2018_cube_split_1-budget_10_seed_42-masked_mlp_classifier-cube_split_1:Jun12 \
+    train_shim2018_cube_split_1-budget_3_seed_42-builtin:Jun12 \
+    train_shim2018_cube_split_1-budget_5_seed_42-builtin:Jun12 \
+    train_shim2018_cube_split_1-budget_10_seed_42-builtin:Jun12 \
+    \
+    train_randomdummy_cube_split_1-budget_3_seed_42-masked_mlp_classifier-cube_split_1:Jun12 \
+    train_randomdummy_cube_split_1-budget_5_seed_42-masked_mlp_classifier-cube_split_1:Jun12 \
+    train_randomdummy_cube_split_1-budget_10_seed_42-masked_mlp_classifier-cube_split_1:Jun12 \
+    train_randomdummy_cube_split_1-budget_3_seed_42-builtin:Jun12 \
+    train_randomdummy_cube_split_1-budget_5_seed_42-builtin:Jun12 \
+    train_randomdummy_cube_split_1-budget_10_seed_42-builtin:Jun12 \
+    \
+    train_shim2018_cube_split_2-budget_3_seed_42-masked_mlp_classifier-cube_split_2:Jun12 \
+    train_shim2018_cube_split_2-budget_5_seed_42-masked_mlp_classifier-cube_split_2:Jun12 \
+    train_shim2018_cube_split_2-budget_10_seed_42-masked_mlp_classifier-cube_split_2:Jun12 \
+    train_shim2018_cube_split_2-budget_3_seed_42-builtin:Jun12 \
+    train_shim2018_cube_split_2-budget_5_seed_42-builtin:Jun12 \
+    train_shim2018_cube_split_2-budget_10_seed_42-builtin:Jun12 \
+    \
+    train_randomdummy_cube_split_2-budget_3_seed_42-masked_mlp_classifier-cube_split_2:Jun12 \
+    train_randomdummy_cube_split_2-budget_5_seed_42-masked_mlp_classifier-cube_split_2:Jun12 \
+    train_randomdummy_cube_split_2-budget_10_seed_42-masked_mlp_classifier-cube_split_2:Jun12 \
+    train_randomdummy_cube_split_2-budget_3_seed_42-builtin:Jun12 \
+    train_randomdummy_cube_split_2-budget_5_seed_42-builtin:Jun12 \
+    train_randomdummy_cube_split_2-budget_10_seed_42-builtin:Jun12 \
+    \
+    train_shim2018_MNIST_split_1-budget_10_seed_42-masked_mlp_classifier-MNIST_split_1:Jun12 \
+    train_shim2018_MNIST_split_1-budget_50_seed_42-masked_mlp_classifier-MNIST_split_1:Jun12 \
+    train_shim2018_MNIST_split_1-budget_100_seed_42-masked_mlp_classifier-MNIST_split_1:Jun12 \
+    train_shim2018_MNIST_split_1-budget_10_seed_42-builtin:Jun12 \
+    train_shim2018_MNIST_split_1-budget_50_seed_42-builtin:Jun12 \
+    train_shim2018_MNIST_split_1-budget_100_seed_42-builtin:Jun12 \
+    \
+    train_randomdummy_MNIST_split_1-budget_10_seed_42-masked_mlp_classifier-MNIST_split_1:Jun12 \
+    train_randomdummy_MNIST_split_1-budget_50_seed_42-masked_mlp_classifier-MNIST_split_1:Jun12 \
+    train_randomdummy_MNIST_split_1-budget_100_seed_42-masked_mlp_classifier-MNIST_split_1:Jun12 \
+    train_randomdummy_MNIST_split_1-budget_10_seed_42-builtin:Jun12 \
+    train_randomdummy_MNIST_split_1-budget_50_seed_42-builtin:Jun12 \
+    train_randomdummy_MNIST_split_1-budget_100_seed_42-builtin:Jun12 \
+    \
+    train_shim2018_MNIST_split_2-budget_10_seed_42-masked_mlp_classifier-MNIST_split_2:Jun12 \
+    train_shim2018_MNIST_split_2-budget_50_seed_42-masked_mlp_classifier-MNIST_split_2:Jun12 \
+    train_shim2018_MNIST_split_2-budget_100_seed_42-masked_mlp_classifier-MNIST_split_2:Jun12 \
+    train_shim2018_MNIST_split_2-budget_10_seed_42-builtin:Jun12 \
+    train_shim2018_MNIST_split_2-budget_50_seed_42-builtin:Jun12 \
+    train_shim2018_MNIST_split_2-budget_100_seed_42-builtin:Jun12 \
+    \
+    train_randomdummy_MNIST_split_2-budget_10_seed_42-masked_mlp_classifier-MNIST_split_2:Jun12 \
+    train_randomdummy_MNIST_split_2-budget_50_seed_42-masked_mlp_classifier-MNIST_split_2:Jun12 \
+    train_randomdummy_MNIST_split_2-budget_100_seed_42-masked_mlp_classifier-MNIST_split_2:Jun12 \
+    train_randomdummy_MNIST_split_2-budget_10_seed_42-builtin:Jun12 \
+    train_randomdummy_MNIST_split_2-budget_50_seed_42-builtin:Jun12 \
+    train_randomdummy_MNIST_split_2-budget_100_seed_42-builtin:Jun12
         
 uv run scripts/plotting/plot_results.py eval_artifact_names=[(string join , $eval_artifact_names)]
