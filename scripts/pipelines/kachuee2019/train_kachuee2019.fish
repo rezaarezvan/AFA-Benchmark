4954f7738990734c0503060386f75c1a42ae624e
Valter Schütz <valterschutz@proton.me>
2025-07-04 11:53:56 +0200

    feat: fish training script for kachuee2019
    .

--- scripts/pipelines/kachuee2019/train_kachuee2019.fish
+++ scripts/pipelines/kachuee2019/train_kachuee2019.fish
@@ -6,12 +6,12 @@
 # Parse args
 # -----------------------
 
-argparse "dataset=+" "budgets=+" "split=+" "help" "launcher=?" "device=?" "speed=?" "pretrain-alias=?" "output-alias=?" "wandb-entity=?" "wandb-project=?" -- $argv
+argparse "dataset=+" "budgets=+" "split=+" "help" "launcher=?" "device=?" "dataset-alias=?" "output-alias=?" "wandb-entity=?" "wandb-project=?" -- $argv
 or exit 1
 
 # Print help if specified
 if set -ql _flag_help
-    echo "Usage: train_shim2018.fish --dataset=<str> --budgets=<str> --split=<int> [--help] [--launcher={custom_slurm,basic}] [--device={cuda,cpu}] [--speed={slow,medium,fast}] [--pretrain-alias=<str>] [--output-alias=<str>] [--wandb-entity=<str>] [--wandb-project=<str>]" >&2
+    echo "Usage: train_kachuee2019.fish --dataset=<str> --budgets=<str> --split=<int> [--help] [--launcher={custom_slurm,basic}] [--device={cuda,cpu}] [--dataset-alias=<str>] [--output-alias=<str>] [--wandb-entity=<str>] [--wandb-project=<str>]" >&2
     exit 1
 end
 
@@ -45,27 +45,13 @@ set -l device cuda
 set -q _flag_device
 and set device $_flag_device
 
-set -l speed slow
-set -q _flag_speed
-and set speed $_flag_speed
-
-set -l pretrain_alias tmp
-set -q _flag_pretrain_alias
-and set pretrain_alias $_flag_pretrain_alias
+set -l dataset_alias tmp
+set -q _flag_dataset_alias
+and set dataset_alias $_flag_dataset_alias
 
 set -l output_alias tmp
 set -q _flag_output_alias
 and set output_alias $_flag_output_alias
-
-set -l speed_suffix
-if test "$speed" = "slow"
-    set speed_suffix ""
-else if test "$speed" = ""
-    echo "Speed argument should either be 'slow', 'medium' or 'fast'"
-    exit 1
-else
-    set speed_suffix "_$speed"
-end
 
 set -l wandb_entity afa-team
 set -q _flag_wandb_entity
@@ -81,18 +67,18 @@ set -gx WANDB_PROJECT $wandb_project
 set -l extra_opts "device=$device hydra/launcher=$launcher"
 
 # --- METHOD TRAINING ---
-echo "Starting shim2018 method training jobs..."
+echo "Starting kachuee2019 method training jobs..."
 sleep 1
 
 set -l jobs
 set -l i 1
 for dataset in $datasets
     set -l dataset_budgets $budgets[$i]
-    set -l pretrained_model_artifact_names
+    set -l dataset_artifact_names
     for split in $splits
-        set -a pretrained_model_artifact_names pretrain_shim2018-{$dataset}_split_$split:$pretrain_alias
+        set -a dataset_artifact_names {$dataset}_split_$split:$dataset_alias
     end
-    set -a jobs "uv run scripts/train_methods/train_shim2018.py -m output_artifact_aliases=[\"$output_alias\"] dataset@_global_=$dataset$speed_suffix pretrained_model_artifact_name="(string join , $pretrained_model_artifact_names)" hard_budget="(string join , $dataset_budgets)" $extra_opts"
+    set -a jobs "uv run scripts/train_methods/train_kachuee2019.py -m output_artifact_aliases=[\"$output_alias\"] dataset@_global_=$dataset dataset_artifact_name="(string join , $dataset_artifact_names)" hard_budget="(string join , $dataset_budgets)" $extra_opts"
     set i (math "$i+1")
 end
 
