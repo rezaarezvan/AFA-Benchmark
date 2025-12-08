9393fdaf333a0146163eb2579486cd203cd31d87
Valter Schütz <valterschutz@proton.me>
2025-12-08 10:25:30 +0100

    feat: create unified registry

--- afabench/common/registry.py
+++ afabench/common/registry.py
@@ -1,12 +1,13 @@
-from afabench.common.custom_types import (
-    AFAClassifier,
-    AFADataset,
-    AFAInitializer,
-    AFAMethod,
-    AFAUnmasker,
-)
+# from afabench.common.custom_types import (
+#     AFAClassifier,
+#     AFADataset,
+#     AFAInitializer,
+#     AFAMethod,
+#     AFAUnmasker,
+# )
 
-AFA_METHOD_CLASSES = {
+REGISTERED_CLASSES = {
+    # AFA Method Classes
     "RLAFAMethod": "afabench.afa_rl.afa_methods.RLAFAMethod",
     "Covert2023AFAMethod": "afabench.afa_discriminative.afa_methods.Covert2023AFAMethod",
     "Gadgil2023AFAMethod": "afabench.afa_discriminative.afa_methods.Gadgil2023AFAMethod",
@@ -16,21 +17,7 @@ AFA_METHOD_CLASSES = {
     "SequentialDummyAFAMethod": "afabench.common.afa_methods.SequentialDummyAFAMethod",
     "RandomDummyAFAMethod": "afabench.common.afa_methods.RandomDummyAFAMethod",
     "OptimalCubeAFAMethod": "afabench.common.afa_methods.OptimalCubeAFAMethod",
-}
-
-
-def get_afa_method_class(class_name: str) -> type[AFAMethod]:
-    """Return a reference to an AFAMethod class, given the class name."""
-    if class_name not in AFA_METHOD_CLASSES:
-        msg = f"Unknown AFA method: {class_name}"
-        raise ValueError(msg)
-
-    module_name, class_name = AFA_METHOD_CLASSES[class_name].rsplit(".", 1)
-    module = __import__(module_name, fromlist=[class_name])
-    return getattr(module, class_name)
-
-
-AFA_DATASET_CLASSES = {
+    # AFA Dataset Classes
     "CubeDataset": "afabench.common.datasets.datasets.CubeDataset",
     "AFAContextDataset": "afabench.common.datasets.datasets.AFAContextDataset",
     "MNISTDataset": "afabench.common.datasets.datasets.MNISTDataset",
@@ -43,21 +30,7 @@ AFA_DATASET_CLASSES = {
     "CKDDataset": "afabench.common.datasets.datasets.CKDDataset",
     "ACTG175Dataset": "afabench.common.datasets.datasets.ACTG175Dataset",
     "ImagenetteDataset": "afabench.common.datasets.datasets.ImagenetteDataset",
-}
-
-
-def get_afa_dataset_class(class_name: str) -> type[AFADataset]:
-    """Return a reference to an AFADataset class, given the class name."""
-    if class_name not in AFA_DATASET_CLASSES:
-        msg = f"Unknown AFA dataset: {class_name}"
-        raise ValueError(msg)
-
-    module_name, class_name = AFA_DATASET_CLASSES[class_name].rsplit(".", 1)
-    module = __import__(module_name, fromlist=[class_name])
-    return getattr(module, class_name)
-
-
-AFA_CLASSIFIER_CLASSES = {
+    # AFA Classifier Classes
     "RandomDummyAFAClassifier": "afabench.common.classifiers.RandomDummyAFAClassifier",
     "UniformDummyAFAClassifier": "afabench.common.classifiers.UniformDummyAFAClassifier",
     "WrappedMaskedMLPClassifier": "afabench.common.classifiers.WrappedMaskedMLPClassifier",
@@ -65,38 +38,10 @@ AFA_CLASSIFIER_CLASSES = {
     "Shim2018AFAClassifier": "afabench.afa_rl.shim2018.models.Shim2018AFAClassifier",
     "Zannone2019AFAClassifier": "afabench.afa_rl.zannone2019.models.Zannone2019AFAClassifier",
     "Kachuee2019AFAClassifier": "afabench.afa_rl.kachuee2019.models.Kachuee2019AFAClassifier",
-}
-
-
-def get_afa_classifier_class(class_name: str) -> type[AFAClassifier]:
-    """Return a reference to an AFAClassifier class, given the class name."""
-    if class_name not in AFA_CLASSIFIER_CLASSES:
-        msg = f"Unknown AFA classifier: {class_name}"
-        raise ValueError(msg)
-
-    module_name, class_name = AFA_CLASSIFIER_CLASSES[class_name].rsplit(".", 1)
-    module = __import__(module_name, fromlist=[class_name])
-    return getattr(module, class_name)
-
-
-AFA_UNMASKER_CLASSES = {
+    # AFA Unmasker Classes
     "DirectUnmasker": "afabench.common.unmaskers.DirectUnmasker",
     "ImagePatchUnmasker": "afabench.common.unmaskers.ImagePatchUnmasker",
-}
-
-
-def get_afa_unmasker_class(class_name: str) -> type[AFAUnmasker]:
-    """Return a reference to an AFAUnmasker class, given the class name."""
-    if class_name not in AFA_UNMASKER_CLASSES:
-        msg = f"Unknown unmasker: {class_name}"
-        raise ValueError(msg)
-
-    module_name, class_name = AFA_UNMASKER_CLASSES[class_name].rsplit(".", 1)
-    module = __import__(module_name, fromlist=[class_name])
-    return getattr(module, class_name)
-
-
-AFA_INITIALIZER_CLASSES = {
+    # AFA Initializer Classes
     "ZeroInitializer": "afabench.common.initializers.ZeroInitializer",
     "FixedRandomInitializer": "afabench.common.initializers.FixedRandomInitializer",
     "DynamicRandomInitializer": "afabench.common.initializers.DynamicRandomInitializer",
@@ -107,19 +52,40 @@ AFA_INITIALIZER_CLASSES = {
 }
 
 
-def get_afa_initializer_class(class_name: str) -> type[AFAInitializer]:
-    """Return a reference to an AFAInitializer class, given the class name."""
-    if class_name not in AFA_INITIALIZER_CLASSES:
-        msg = f"Unknown initializer: {class_name}"
+def get_class(class_name: str) -> type:
+    """Return a reference to a registered class, given the class name."""
+    if class_name not in REGISTERED_CLASSES:
+        msg = f"Unknown class: {class_name}"
         raise ValueError(msg)
 
-    module_name, class_name = AFA_INITIALIZER_CLASSES[class_name].rsplit(
+    module_name, class_name_in_module = REGISTERED_CLASSES[class_name].rsplit(
         ".", 1
     )
-    module = __import__(module_name, fromlist=[class_name])
-    return getattr(module, class_name)
+    module = __import__(module_name, fromlist=[class_name_in_module])
+    return getattr(module, class_name_in_module)
 
 
-def get_class(class_name: str) -> type:
-    # TODO:
-    pass
+# Backward compatibility functions - these delegate to the unified get_class function
+# def get_afa_method_class(class_name: str) -> type[AFAMethod]:
+#     """Return a reference to an AFAMethod class, given the class name."""
+#     return get_class(class_name)
+
+
+# def get_afa_dataset_class(class_name: str) -> type[AFADataset]:
+#     """Return a reference to an AFADataset class, given the class name."""
+#     return get_class(class_name)
+
+
+# def get_afa_classifier_class(class_name: str) -> type[AFAClassifier]:
+#     """Return a reference to an AFAClassifier class, given the class name."""
+#     return get_class(class_name)
+
+
+# def get_afa_unmasker_class(class_name: str) -> type[AFAUnmasker]:
+#     """Return a reference to an AFAUnmasker class, given the class name."""
+#     return get_class(class_name)
+
+
+# def get_afa_initializer_class(class_name: str) -> type[AFAInitializer]:
+#     """Return a reference to an AFAInitializer class, given the class name."""
+#     return get_class(class_name)
